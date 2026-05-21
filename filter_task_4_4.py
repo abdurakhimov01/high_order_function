f1a580e17a0b4a0895eeb0ab2ac6bdfd9582bb89
abdurakhimov01 <sololeveling1958@gmail.com>
2026-05-21 13:16:04 +0500

    ho to get a first letter from a name?

--- filter_task_4_4.py
+++ filter_task_4_4.py
@@ -6,4 +6,6 @@ def filter_task_4_4(names):
     Returns:
         list of names that start with "A"
     """
-    return
+    return names[0]=="A"
+a=["Alice", "Bob", "Anna", "Charlie"]
+print(list(filter(filter_task_4_4,a)))
