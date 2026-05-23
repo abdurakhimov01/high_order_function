bfe818cdd79ad9609009a422d2243e2d66d1457a
abdurakhimov01 <sololeveling1958@gmail.com>
2026-05-23 10:58:35 +0500

    max

--- max_task_2_4.py
+++ max_task_2_4.py
@@ -6,4 +6,15 @@ def max_task_2_4(words):
     Returns:
         word with most vowels
     """
-    return
+    k="aeiouAEIOU"
+    def hi(x):
+        c=0
+        for i in x:
+            if i in k:
+                c+=1
+        return c
+    return max(words,key=hi)
+print(max_task_2_4(words=["tree", "education", "sky", "road"]))
+
+
+
